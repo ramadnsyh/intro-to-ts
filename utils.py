3373d86529839e828363ef77baf970f8aa336a68
mramadnsyh <muhammad.ramadiansyah@finaccel.co>
2023-07-07 08:03:41 +0700

    hello

---
+++ utils.py
@@ -0,0 +1,5 @@
+import numpy as np
+def mean_absolute_percentage_error(y_test, pred):
+    y_test, pred = np.array(y_test), np.array(pred)
+    mape = np.mean(np.abs((y_test - pred) / y_test))
+    return mape
